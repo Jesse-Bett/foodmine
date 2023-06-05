9c2e16ecbd3db7b0aa3d9527a1b0730619acf59a
Jesse-Bett <jessekip21@gmail.com>
2023-06-06 00:46:39 +0300

    Added food details
    Added food ratings,favorites and prices and cooktime based on the 'Food' model

--- src/app/shared/models/Food.ts
+++ src/app/shared/models/Food.ts
@@ -1,11 +1,11 @@
-export class Food{
-    id!:number;  // ? indicates that an id is optional, !indicates that the id is mandatory.
-    name!:string;
-    price!:number;
-    tags?:string[];
-    favorite:boolean = false;
-    stars:number = 0;
-    imageUrl!:string;
-    origins!:string[];
-    cookTime!:string;
+export class Food {
+  id!: number; // ? indicates that an id is optional, !indicates that the id is mandatory.
+  name!: string;
+  price!: number;
+  tags?: string[];
+  favorite: boolean = false;
+  stars: number = 0;
+  imageUrl!: string;
+  origins!: string[];
+  cookTime!: string;
 }
